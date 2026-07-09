1ad0fd0de74fb49c6a3b3cbcd3ef49a0bb7c4bbe
ragavragav430-coder <ragavragav430@gmail.com>
2026-07-09 11:55:27 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -1,13 +1,14 @@
--- Create Student table
+CREATE DATABASE  college;
 
--- StudentID
+use college;
 
--- StudentName
-
--- DOB
-
--- Gender
-
--- DepartmentID
-
--- Add constraints
+CREATE TABLE  student(
+   StudentID INT(5)PRIMARY KEY,
+   StudentName VARCHAR(20)NOT NULL,
+   DOB DATE NOT NULL,
+   Gender VARCHAR(10)NOT NULL,
+   DepartmentID INT(50)NOT NULL
+   );
+  
+  
+  
